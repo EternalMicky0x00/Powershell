7888578b1076df54ad647e7e55101484472ec858
Micky <Mickyddroy@Gmail.com>
2025-07-04 23:12:58 -0400

    Create Command.ps1

---
+++ Command.ps1
@@ -0,0 +1,15 @@
+Get-ExecutionPolicy
+
+powershell -ExecutionPolicy Bypass -File "C:\Path\To\Script.ps1"
+
+powershdll powershell -ep bypass
+
+Add-ADGroupMember -Identity "GroupName" -Members "UserName"
+
+$credential = Get-Credential
+
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+Start-Process powershell.exe -Credential (Get-Credential)
+
+psexec -i -s powershell.exe
