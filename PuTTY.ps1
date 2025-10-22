476e2fd07b623fafc345af37e0f2bc735968064e
Micky <Mickyddroy@Gmail.com>
2025-10-22 10:41:02 -0400

    Delete PuTTY.ps1

--- PuTTY.ps1
+++
@@ -1,8 +0,0 @@
-function RunPuTTY {
-    param (
-        [Parameter(Mandatory=$true)]$TestCommands
-    )
-    Start-Process "Command, $TestCommands"
-}
-$TestCommands=Write-Host Run PuTTY successfully
-Cmd.exe /c "putty.exe groot.underthewire.tech" $TestCommands
