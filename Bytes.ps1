7fb69371fc5a988b4ef481bd7b4033a66daaa32f
Micky <Mickyddroy@Gmail.com>
2025-10-22 23:25:18 -0400

    Update output file path in Bytes.ps1

--- Bytes.ps1
+++ Bytes.ps1
@@ -1,4 +1,5 @@
 $bytes=[byte[]](72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100)
-$outputFile = "C:\Users\Micky\Documents\Bytes.bin"
+$outputFile = "C:\Users\0000\Documents\Bytes.bin"
 [System.IO.File]::WriteAllBytes($outputFile, $bytes)
+
 Write-Host "[+] $outputFile"
